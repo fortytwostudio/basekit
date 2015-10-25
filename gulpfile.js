971af5293d7dd9957cee67a50d51bd33ebad713b
Ryan Roberts <ryan@eqdesign.co.uk>
2015-10-25 19:47:24 +0000

    testing gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ var nano    = require('gulp-cssnano');
 var sourcemaps  = require('gulp-sourcemaps');
 
 gulp.task('scss', function() {
-  gulp.src('/css/basekit.scss')
+  gulp.src('css/basekit.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.init())
     .pipe(nano({autoprefixer: {browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1']}}))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('/css'));
+    .pipe(gulp.dest('css'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('/css/**/*.scss', ['scss']);
+gulp.task('watch', ['scss'], function() {
+  gulp.watch('css/**/*.scss', ['scss']);
 });
 
 gulp.task('default', ['watch']);
