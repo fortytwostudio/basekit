e7282fd9c53be8ebb9a30e4f4582e0ae062c437a
Ryan Roberts <ryan@eqdesign.co.uk>
2015-10-26 19:55:31 +0000

    added gulp size

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,19 @@ var gulp        = require('gulp'),
     nano        = require('gulp-cssnano'),
     deploy      = require('gulp-gh-pages'),
     concat      = require('gulp-concat'),
-    uglify      = require('gulp-uglify');
+    uglify      = require('gulp-uglify'),
+    imagemin    = require('gulp-imagemin'),
+    pngquant    = require('imagemin-pngquant'),
+    size        = require('gulp-size');
 
 // Compile Sass with autoprefixer, I've removed sourcemaps
 gulp.task('scss', function() {
   gulp.src('css/basekit.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(nano({autoprefixer: {browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1']}}))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css'))
+    .pipe(size({ gzip: false, showFiles: true }))
+    .pipe(size({ gzip: true, showFiles: true }));
 });
 
 // Easy deploy to gh-pages branch
@@ -22,13 +27,26 @@ gulp.task('deploy', function () {
 // Minify and concat the js files for use
 gulp.task('js', function() {
   gulp.src('js/*.js')
-    .pipe(uglify())
     .pipe(concat('basekit.js'))
-    .pipe(gulp.dest('js/min'));
+    .pipe(uglify())
+    .pipe(gulp.dest('js/min'))
+    .pipe(size({ gzip: true, showFiles: true }));
 });
 
-gulp.task('watch', ['scss', 'js'], function() {
+gulp.task('img', function () {
+  return gulp.src('imgs/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      multipass: true,
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('imgs'));
+});
+
+gulp.task('watch', function() {
   gulp.watch('css/**/*.scss', ['scss']);
+  gulp.watch('js/*.js', ['js']);
 });
 
 gulp.task('default', ['watch']);
