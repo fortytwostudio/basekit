bc4d4218e60cdc8e488b9ca534dd48d127218dd9
Ryan Roberts <ryan@eqdesign.co.uk>
2015-10-26 18:41:52 +0000

    added gulp-gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
-    nano        = require('gulp-cssnano');
+    nano        = require('gulp-cssnano'),
+    deploy      = require('gulp-gh-pages');
 
 gulp.task('scss', function() {
   gulp.src('css/basekit.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(nano({autoprefixer: {browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1']}}))
     .pipe(gulp.dest('css'));
+});
+
+gulp.task('deploy', function () {
+  return gulp.src('**/*')
+    .pipe(deploy());
 });
 
 gulp.task('watch', ['scss'], function() {
