b2b920eb7bdad95f54f26f35eb6ff3dc420a0a51
Ryan Roberts <ryan@eqdesign.co.uk>
2015-10-28 17:24:14 +0000

    removed rename plugin and compiled all html

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
-    rename      = require('gulp-rename'),
     nano        = require('gulp-cssnano'),
     deploy      = require('gulp-gh-pages'),
     concat      = require('gulp-concat'),
@@ -48,10 +47,8 @@ gulp.task('img', function () {
 
 // Minify HTML source and rename the index-dev file
 gulp.task('html', function() {
-  gulp.src('index-dev.html')
-  .pipe(rename('index-dev.html'))
+  gulp.src(['**/*.html', '!**/*.dev.html'])
   .pipe(minifyHTML({cdata: true, conditionals: true, quotes: true, spare: true}))
-  .pipe(rename({ basename: "index" }))
   .pipe(gulp.dest('./'));
 });
 
