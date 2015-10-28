4af76fdbf1773e6deb16edf8de4e07dc4c24bba3
Ryan Roberts <ryan@eqdesign.co.uk>
2015-10-28 17:27:10 +0000

    Update 2015-10-28T17:24:42.111Z

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp        = require('gulp'),
     uglify      = require('gulp-uglify'),
     imagemin    = require('gulp-imagemin'),
     pngquant    = require('imagemin-pngquant'),
+    minifyHTML  = require('gulp-minify-html'),
     size        = require('gulp-size');
 
 // Compile Sass with autoprefixer, I've removed sourcemaps
@@ -14,11 +15,10 @@ gulp.task('scss', function() {
     .pipe(sass().on('error', sass.logError))
     .pipe(nano({autoprefixer: {browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1']}}))
     .pipe(gulp.dest('css'))
-    .pipe(size({ gzip: false, showFiles: true }))
-    .pipe(size({ gzip: true, showFiles: true }));
+    .pipe(size({ /* gzip: true,*/ showFiles: true }));
 });
 
-// Easy deploy to gh-pages branch
+// Easy deploy to the gh-pages branch
 gulp.task('deploy', function () {
   return gulp.src('**/*')
     .pipe(deploy());
@@ -30,9 +30,10 @@ gulp.task('js', function() {
     .pipe(concat('basekit.js'))
     .pipe(uglify())
     .pipe(gulp.dest('js/min'))
-    .pipe(size({ gzip: true, showFiles: true }));
+    .pipe(size({ /* gzip: true,*/ showFiles: true }));
 });
 
+// Optimise images
 gulp.task('img', function () {
   return gulp.src('imgs/*')
     .pipe(imagemin({
@@ -44,9 +45,18 @@ gulp.task('img', function () {
     .pipe(gulp.dest('imgs'));
 });
 
+// Minify HTML source and rename the index-dev file
+gulp.task('html', function() {
+  gulp.src(['**/*.html', '!**/*.dev.html'])
+  .pipe(minifyHTML({cdata: true, conditionals: true, quotes: true, spare: true}))
+  .pipe(gulp.dest('./'));
+});
+
+// Only watch Sass and JS files
 gulp.task('watch', function() {
   gulp.watch('css/**/*.scss', ['scss']);
   gulp.watch('js/*.js', ['js']);
+  gulp.watch('index-dev.html', ['html']);
 });
 
 gulp.task('default', ['watch']);
