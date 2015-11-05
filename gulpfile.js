de74b337d1a5d93b695fb9f24d943c394f7119e9
Ryan Roberts <ryan@eqdesign.co.uk>
2015-11-05 11:36:27 +0000

    Update 2015-11-05T11:33:10.916Z

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
-var gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
-    nano        = require('gulp-cssnano'),
-    deploy      = require('gulp-gh-pages'),
-    concat      = require('gulp-concat'),
-    uglify      = require('gulp-uglify'),
-    imagemin    = require('gulp-imagemin'),
-    pngquant    = require('imagemin-pngquant'),
-    minifyHTML  = require('gulp-minify-html'),
-    size        = require('gulp-size');
+var gulp          = require('gulp'),
+    sass          = require('gulp-sass'),
+    nano          = require('gulp-cssnano'),
+    deploy        = require('gulp-gh-pages'),
+    concat        = require('gulp-concat'),
+    uglify        = require('gulp-uglify'),
+    imagemin      = require('gulp-imagemin'),
+    pngquant      = require('imagemin-pngquant'),
+    minifyHTML    = require('gulp-minify-html'),
+    minifyInline  = require('gulp-minify-inline'),
+    size          = require('gulp-size');
 
 // Compile Sass with autoprefixer, I've removed sourcemaps
 gulp.task('scss', function() {
@@ -15,7 +16,7 @@ gulp.task('scss', function() {
     .pipe(sass().on('error', sass.logError))
     .pipe(nano({autoprefixer: {browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1']}}))
     .pipe(gulp.dest('css'))
-    .pipe(size({ /* gzip: true,*/ showFiles: true }));
+    .pipe(size({ gzip: true, showFiles: true }));
 });
 
 // Easy deploy to the gh-pages branch
@@ -30,7 +31,7 @@ gulp.task('js', function() {
     .pipe(concat('basekit.js'))
     .pipe(uglify())
     .pipe(gulp.dest('js/min'))
-    .pipe(size({ /* gzip: true,*/ showFiles: true }));
+    .pipe(size({ gzip: true, showFiles: true }));
 });
 
 // Optimise images
@@ -47,16 +48,17 @@ gulp.task('img', function () {
 
 // Minify HTML source and rename the index-dev file
 gulp.task('html', function() {
-  gulp.src(['**/*.html', '!**/*.dev.html'])
-  .pipe(minifyHTML({cdata: true, conditionals: true, quotes: true, spare: true}))
-  .pipe(gulp.dest('./'));
+  gulp.src('./html/*.html')
+    .pipe(minifyHTML({cdata: true}))
+    .pipe(minifyInline())
+    .pipe(gulp.dest('./'));
 });
 
 // Only watch Sass and JS files
 gulp.task('watch', function() {
   gulp.watch('css/**/*.scss', ['scss']);
   gulp.watch('js/*.js', ['js']);
-  gulp.watch('index-dev.html', ['html']);
+  gulp.watch('./html/*.html', ['html']);
 });
 
 gulp.task('default', ['watch']);
