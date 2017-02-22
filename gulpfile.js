4496b1bb55a8e88626f32dd0bda3c8df9b9559e4
Ryan Roberts <ryanr242@gmail.com>
2017-02-22 11:26:24 +0000

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -111,13 +111,14 @@ gulp.task('sassdoc', function () {
 // Combine various functions into watch
 gulp.task('watch', function() {
   gulp.watch('css/**/*.scss', ['scss']);
-  gulp.watch('js/*.js', ['js']);
   gulp.watch('html/*.html', ['html']);
+  gulp.watch('js/*.js', ['js']);
 });
 
 // Combine various functions into watch + twig
 gulp.task('watch-twig', function() {
   gulp.watch('css/**/*.scss', ['scss']);
+  gulp.watch('html/*.html', ['html']);
   gulp.watch('js/*.js', ['js']);
   gulp.watch('./templates/**/*', ['twig']);
 });
