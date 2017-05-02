6649c6d12046eedb78c3ef75d0cd553974a6ed7b
Ryan Roberts <ryanr242@gmail.com>
2017-05-02 18:18:23 +0100

    Added cookie saving to font chooser

--- js/basekit-custom.js
+++ js/basekit-custom.js
@@ -14,4 +14,18 @@ $(function() {
   // $.smoothPageScrollByLoaded();
   /// Basic config with Add Hash disabled
   $('a[href^="#"]').smoothPageScroll();
+
+  // Funcy font tester tool
+  $('body').on('keypress',"#fontoggler", function(event){
+    if (event.which == 13){
+      $('body').css('font-family', $(this).val());
+      $.cookie("fontoggler", '', { path: '/', expires: 1 });
+      $.cookie("fontoggler", $(this).val(), { path: '/', expires: 1 });
+    };
+  });
+
+  if ($.cookie("fontoggler") != undefined) {
+    $('body').css('font-family', $.cookie("fontoggler"));
+    $('#fontoggler').attr('value', $.cookie("fontoggler"));
+  };
 });
