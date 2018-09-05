981d0a165f36c9fb17dbe3b09e3a8d04fd2313cd
Ryan Roberts <ryanr242@gmail.com>
2018-09-05 22:18:13 +0100

    streamlined gulpfile plus some additions

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,37 @@
-const pkg = require("./package.json");
+const pkg   = require("./package.json");
+const gulp  = require('gulp');
 
-const gulp    = require('gulp');
+// load all plugins in "devDependencies" into the variable $
+const $ = require("gulp-load-plugins")({
+  pattern: ["*"],
+  scope: ["devDependencies"]
+});
 
-var data    = require('gulp-data'); // Data storage for Twig templates and Sass
-var twig    = require('gulp-twig');
-var sass    = require('gulp-sass');
-var nano    = require('gulp-cssnano'); // Nano for optimising and post-processing CSS
-var concat  = require('gulp-concat'); // Join JS file into a single file
-var uglify  = require('gulp-uglify'); // Minify that single JS file
-var htmlmin = require('gulp-htmlmin'); // Minify and clean up HTML files
-var size    = require('gulp-size'); // Report file sizes in the CLI
-
+const banner = [
+    "/**",
+    " * @project        <%= pkg.name %>",
+    " * @author         <%= pkg.author %>",
+    " * @version        <%= pkg.version %>",
+    " * @build          " + $.moment().format("llll") + " ET",
+    " * @release        " + $.gitRevSync.long() + " [" + $.gitRevSync.branch() + "]",
+    " *",
+    " */",
+    ""
+].join("\n");
 
 // PATHS
 // ————————————————————————————————————————————————————————————————————————————————————
-var sassSrcPath   = './src/sass';
-var twigSrcPath   = './src/twig';
-var jsSrcPath     = './src/js';
+const sassSrcPath   = pkg.paths.src.sass;
+const twigSrcPath   = pkg.paths.src.twig;
+const jsSrcPath     = pkg.paths.src.js;
 
-var publicPath    = './public/';
-var htmlDestPath  = publicPath + 'demo';
-var cssDestPath   = publicPath + 'assets/css';
-var jsDestPath    = publicPath + 'assets/js';
+const htmlDestPath  = pkg.paths.dist.html;
+const cssDestPath   = pkg.paths.dist.css;
+const jsDestPath    = pkg.paths.dist.js;
 
+const sassName        = pkg.vars.scssName;
+const cssName        = pkg.vars.cssName;
+const jsName        = pkg.vars.jsName;
 
 // CACHING
 // ————————————————————————————————————————————————————————————————————————————————————
@@ -36,33 +45,37 @@ function requireUncached($module) {
 // SASS/CSS
 // ————————————————————————————————————————————————————————————————————————————————————
 // Compile Sass (with Nano and Autoprefixer)
-gulp.task('sass', function() {
-  gulp.src(sassSrcPath + '/basekit.scss')
-    .pipe(data(function(file){ return requireUncached('./data.json'); }))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(nano({
+gulp.task("sass", () => {
+  return gulp.src(sassSrcPath + sassName)
+    .pipe($.data(function(file){ return requireUncached('./data.json'); }))
+    .pipe($.sass().on('error', $.sass.logError))
+    .pipe($.cssnano({
       minifySelectors: false, // This was interfering with the global selector so I've disabled it: http://cssnano.co/optimisations/minifySelectors/
       autoprefixer: {
         add: true, // Enable adding browser prefixes
         browsers: [ '> 0.5% in GB', 'last 3 versions', 'ie >= 9' ] // Browser support: must be 0.5% usage in UK, going back 3 versions, but make sure IE is not dropped off
       }
     }))
-    .pipe(gulp.dest(cssDestPath))
-    .pipe(size({ showFiles: true })) // Show file size
-    .pipe(size({ gzip: true, showFiles: true })); // Show file size after gzip
+    .pipe($.header(banner, {pkg: pkg}))
+
+    .pipe($.gulp.dest(cssDestPath))
+    .pipe($.size({ showFiles: true })) // Show file size
+    .pipe($.size({ gzip: true, showFiles: true })); // Show file size after gzip
 });
 
 
 // JAVASCRIPT
 // ————————————————————————————————————————————————————————————————————————————————————
 // Minify and combine javascript files for production, unless they start with an _
-gulp.task('js', function() {
-  gulp.src(jsSrcPath + '/[^_]*.js') // ignore underscored files
-    .pipe(concat('basekit.min.js')) // Combine all (none _) js files into this file
-    .pipe(uglify()) // Minify the file
-    .pipe(gulp.dest(jsDestPath)) // Output it here
-    .pipe(size({ showFiles: true })) // Show file size before gzip
-    .pipe(size({ gzip: true, showFiles: true }) // Show file size after gzip
+gulp.task('js', () => {
+  return gulp.src(jsSrcPath + '[^_]*.js') // ignore underscored files
+    .pipe($.concat(jsName)) // Combine all (none _) js files into this file
+    .pipe($.uglify()) // Minify the file
+    .pipe($.header(banner, {pkg: pkg}))
+
+    .pipe($.gulp.dest(jsDestPath)) // Output it here
+    .pipe($.size({ showFiles: true })) // Show file size before gzip
+    .pipe($.size({ gzip: true, showFiles: true }) // Show file size after gzip
   );
 });
 
@@ -71,11 +84,11 @@ gulp.task('js', function() {
 // ————————————————————————————————————————————————————————————————————————————————————
 // Compile Twig templates to an static frontend for demonstration.
 // This is quite long-winded and not ideal, but does the job for now
-gulp.task('twig', function() {
-  return gulp.src(twigSrcPath + '/**/[^_]*.twig') // run the Twig template parser on .twig files that don't start with an _
-  .pipe(data(function(file){ return requireUncached('./data.json'); })) // Uncached data for populating Twig files
-  .pipe(twig({ base: twigSrcPath, cache: false })) // Let gulp-twig know where the base template directory is
-  .pipe(htmlmin({
+gulp.task('twig', () => {
+  return gulp.src(twigSrcPath + '**/[^_]*.twig') // run the Twig template parser on .twig files that don't start with an _
+  .pipe($.data(function(file){ return requireUncached('./data.json'); })) // Uncached data for populating Twig files
+  .pipe($.twig({ base: twigSrcPath, cache: false })) // Let gulp-twig know where the base template directory is
+  .pipe($.htmlmin({
     collapseWhitespace: true,
     removeComments: true,
     removeAttributeQuotes: true,
@@ -87,20 +100,20 @@ gulp.task('twig', function() {
     minifyJS: true,
     minifyCSS: true
   })) // Minify the files for development use
-  .pipe(gulp.dest(htmlDestPath)) // Output minified file to public directory
+  .pipe($.gulp.dest(htmlDestPath)) // Output minified file to public directory
 });
 
 
 // WRAP INTO WATCH TASK
 // ————————————————————————————————————————————————————————————————————————————————————
-gulp.task('watch', function() {
-  // gulp.watch(sassSrcPath + '/**/*.scss', { interval: 500 }, ['sass']);
-  gulp.watch(sassSrcPath + '/**/*.scss', ['sass']);
-  gulp.watch(jsSrcPath + '/*.js', ['js']);
-  gulp.watch([twigSrcPath + '/**/*.twig', twigSrcPath + '/**/*.json'], ['twig']);
+gulp.task("default", () => {
+  gulp.watch(sassSrcPath + '**/*.scss', ['sass']);
+  gulp.watch(jsSrcPath + '*.js', ['js']);
+  gulp.watch([twigSrcPath + '**/*.twig', twigSrcPath + '**/*.json'], ['twig']);
 });
 
-
-// SET WATCH AS DEFAULT
+// WATCH ONLY SASS CHANGES
 // ————————————————————————————————————————————————————————————————————————————————————
-gulp.task('default', ['watch']);
+gulp.task('watch-sass', function() {
+  gulp.watch(sassSrcPath + '**/*.scss', ['sass']);
+});
