d913f83197a2447e2db086f9f900289d84768ae0
Ryan Roberts <ryanr242@gmail.com>
2018-09-12 13:23:48 +0100

    removed major from browser versions

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ gulp.task("sass", () => {
       minifySelectors: false, // This was interfering with the global selector so I've disabled it: http://cssnano.co/optimisations/minifySelectors/
       autoprefixer: {
         add: true, // Enable adding browser prefixes
-        browsers: [ '> 0.5% in GB', 'last 3 major versions', 'not ie 9' ] // Browser support: must be 0.5% usage in UK, going back 3 versions, but make sure IE is not dropped off
+        browsers: [ '> 0.5% in GB', 'last 3 versions', 'not ie 9' ] // Browser support: must be 0.5% usage in UK, going back 3 versions, but make sure IE is not dropped off
         // http://browserl.ist/?q=%3E+0.5%25+in+GB%2C+last+3+major+versions%2C+not+ie+9&chrome_dont_add_custom_search_engines_srsly=
       }
     }))
