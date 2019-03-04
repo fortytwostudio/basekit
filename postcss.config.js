a492222531db28a8e74223f60c423bc5974c14b5
Ryan Roberts <ryanr242@gmail.com>
2019-03-04 15:06:47 +0000

    cleanup yo

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   plugins: {
+    cssnano: {
+      minifySelectors: false
+    },
     autoprefixer: {
-      browsers: [ '> 0.5% in GB', 'last 3 versions', 'ie >= 9' ]
+      browsers: [ '> 0.5% in GB', 'last 3 versions', 'not ie 9' ]
     }
   }
 }
