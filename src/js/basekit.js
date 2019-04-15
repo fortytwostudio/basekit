39f12dd8dd85b88bb2e1743b7d52a90ec028593b
Ryan Roberts <ryanr242@gmail.com>
2019-04-15 21:53:39 +0100

    Dropped much of the query for easy toggle state js

--- src/js/basekit.js
+++ src/js/basekit.js
@@ -1,14 +1,15 @@
 (function($) {
+  // No need to use this.
   /// Very simple class toggle for the hamburger icon
-  $('#nav-toggler').on('click', function (event) {
-    event.preventDefault()
-    $('#nav-toggler, #nav-primary').toggleClass('is-active');
-  });
-  $(document).click(function(event) {
-    if (!$(event.target).closest('#nav-toggler, #nav-primary').length)  {
-      $('#nav-toggler, #nav-primary').removeClass('is-active');
-    }
-  });
+  // $('#nav-toggler').on('click', function (event) {
+  //   event.preventDefault()
+  //   $('#nav-toggler, #nav-primary').toggleClass('is-active');
+  // });
+  // $(document).click(function(event) {
+  //   if (!$(event.target).closest('#nav-toggler, #nav-primary').length)  {
+  //     $('#nav-toggler, #nav-primary').removeClass('is-active');
+  //   }
+  // });
 
   // Smart Menus script yo
   // https://www.smartmenus.org/docs/
@@ -24,41 +25,41 @@
 
   /// See https://github.com/kamem/jquery.smoothPageScroll
   // Smooth scroll any links with a #
-  $('a[href*="#"]').smoothPageScroll();
+  // $('a[href*="#"]').smoothPageScroll();
   // Use this option with the ^ if targetting all links with a # breaks stuff
   // $('a[href^="#"]').smoothPageScroll();
   // Pages will smooth scroll on load if # is present in URL
   // $.smoothPageScrollByLoaded();
 
 
-  $('.accordion-title').click(function() {
-    $content = $('.accordion-content');
-    $titles = $('.accordion-title');
-    $this = $(this);
-    $target = $this.next();
-    // $target =  $this.parent().next();
-    $easing = { duration: 600, easing: "easeInOutCubic" };
-
-    if($this.hasClass('accordion-active')){
-      $this.removeClass('accordion-active');
-    }else{
-      $titles.removeClass('accordion-active');
-      $this.addClass('accordion-active');
-    }
-
-    if($target.hasClass('accordion-open')){
-      $target.removeClass('accordion-open').slideUp($easing);
-    }else{
-      $content.removeClass('accordion-open').slideUp($easing);
-      $target.addClass('accordion-open').slideDown($easing);
-    }
-    return false;
-  });
-
-  $('.accordion-title').on('keydown', function(e) {
-    if (e.keyCode == 13 || e.keyCode == 32) {
-        $(this).trigger("click");
-    }
-  });
+  // $('.accordion-title').click(function() {
+  //   $content = $('.accordion-content');
+  //   $titles = $('.accordion-title');
+  //   $this = $(this);
+  //   $target = $this.next();
+  //   // $target =  $this.parent().next();
+  //   $easing = { duration: 600, easing: "easeInOutCubic" };
+  //
+  //   if($this.hasClass('accordion-active')){
+  //     $this.removeClass('accordion-active');
+  //   }else{
+  //     $titles.removeClass('accordion-active');
+  //     $this.addClass('accordion-active');
+  //   }
+  //
+  //   if($target.hasClass('accordion-open')){
+  //     $target.removeClass('accordion-open').slideUp($easing);
+  //   }else{
+  //     $content.removeClass('accordion-open').slideUp($easing);
+  //     $target.addClass('accordion-open').slideDown($easing);
+  //   }
+  //   return false;
+  // });
+  //
+  // $('.accordion-title').on('keydown', function(e) {
+  //   if (e.keyCode == 13 || e.keyCode == 32) {
+  //       $(this).trigger("click");
+  //   }
+  // });
 
 })(jQuery);
